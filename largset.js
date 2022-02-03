0f290a8743b5da2a9906536c7409c29ed3f870e0
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-03 14:05:09 +0600

    second commit

---
+++ largset.js
@@ -0,0 +1,17 @@
+var number1 = 10;
+var number2 = 20;
+var number3 = 30;
+if (number1 > number2) {
+    if (number1 > number3) {
+        console.log("Number one is larger");  
+    }
+    
+}
+else if(number2 > number3){
+    if (number2 > number1) {
+        console.log("number two is larger");
+    }
+}
+else{
+    console.log("Number three is larger");
+}
