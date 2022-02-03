cc51e552d96818867d99f8e1d6e82bae610d5451
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-03 13:46:30 +0600

    first commit

---
+++ vowel.js
@@ -0,0 +1,8 @@
+var input = "A";
+var letter = input.toLowerCase();
+if (letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u") {
+    console.log(letter,"is vowel");
+}
+else{
+    console.log(letter,"is not vowel");
+}
