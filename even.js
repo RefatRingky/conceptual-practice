75d874773e1aaafc59071f02a6bea4a0de0a1317
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-03 14:18:22 +0600

    final commit 2

--- even.js
+++ even.js
@@ -1,5 +1,12 @@
+// for(var i = 0; i<=100; i++){
+//     if (i%2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// 1-100 odd number print
 for(var i = 0; i<=100; i++){
-    if (i%2 == 0) {
+    if (i%2 != 0) {
         console.log(i);
     }
 }
