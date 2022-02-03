50c94523373d5628c8b7819196e3fdabb7943402
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-03 16:42:38 +0600

    feet commit

---
+++ feetToinch.js
@@ -0,0 +1,16 @@
+function feetToinch(feet) {
+    var totalInches = feet * 12;
+    return totalInches;
+
+}
+let  inches = feetToinch(10);
+console.log(inches);
+
+// // centemeter to meter
+// function centemeterToMeter(centemeter) {
+//     var totalMeter = centemeter / 100;
+//     return totalMeter;
+
+// }
+// let  centemeter = centemeterToMeter(10);
+// console.log(centemeter);
