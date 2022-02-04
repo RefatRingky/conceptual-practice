f61a255cbeb1325153a70c34aa0f1ff055c9bbb2
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-04 11:46:22 +0600

    final commit

---
+++ arraySum.js
@@ -0,0 +1,7 @@
+var number = [44,45,67,60,34,55,32];
+var sum = 0;
+for(var i = 0; i< number.length; i++){
+    var element = number[i];
+    sum = sum + element;
+}
+console.log("Total sum is:",sum);
